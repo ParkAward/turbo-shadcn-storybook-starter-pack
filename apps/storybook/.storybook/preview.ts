10854e2ee0fa36bd6dbfef8ea1d8390c7a1b978f
sangjun.park <sangjun.park@gintlab.com>
2024-08-29 12:12:30 +0900

    initial setup about turborepo-shadcn-storybook

---
+++ apps/storybook/.storybook/preview.ts
@@ -0,0 +1,27 @@
+import type { Preview } from "@storybook/react";
+import "@repo/ui/styles/globals.css";
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
+};
+
+export default preview;
---
+++ packages/ui/index.tsx
@@ -0,0 +1,2 @@
+export { cn } from "@ui/lib/utils";
+export { Button } from "@ui/components/ui/button";
